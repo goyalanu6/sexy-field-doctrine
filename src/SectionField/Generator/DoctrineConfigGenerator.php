16cfe57a475544419d66d342aec332e8d9c499f4
Dion Snoeijen <hallo@dionsnoeijen.nl>
2017-10-06 12:14:52 +0200

    Updated default storage location to mirror symfony's default

--- src/SectionField/Generator/DoctrineConfigGenerator.php
+++ src/SectionField/Generator/DoctrineConfigGenerator.php
@@ -56,7 +56,7 @@ class DoctrineConfigGenerator extends Generator implements GeneratorInterface
 
         return Writable::create(
             (string) $this->generateXml(),
-            $this->sectionConfig->getNamespace() . '\\config\\xml\\',
+            $this->sectionConfig->getNamespace() . 'Resources\\config\\doctrine\\',
             str_replace('\\', '.', $this->sectionConfig->getNamespace()) .
             '.Entity.' . ucfirst((string) $this->sectionConfig->getHandle()) . '.dcm.xml'
         );
