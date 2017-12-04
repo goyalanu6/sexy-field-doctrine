3467959c88c5adfc85bdb497e1879384763f0daf
Marieke Bednarczyk <mbed@live.nl>
2017-12-04 10:20:01 +0100

    fix bug where class_exists was given a class instead of a class name

--- test/unit/SectionField/Generator/DoctrineConfigGeneratorTest.php
+++ test/unit/SectionField/Generator/DoctrineConfigGeneratorTest.php
@@ -9,15 +9,18 @@ use PHPUnit\Framework\TestCase;
 use Psr\Container\ContainerInterface;
 use Tardigrades\Entity\Field;
 use Tardigrades\Entity\FieldType;
+use Tardigrades\Entity\FieldTypeInterface;
 use Tardigrades\Entity\Section;
+use Tardigrades\FieldType\Relationship\Generator\DoctrineOneToOneGenerator;
 use Tardigrades\SectionField\Generator\Writer\Writable;
 use Tardigrades\SectionField\Service\FieldManagerInterface;
 use Tardigrades\SectionField\Service\FieldTypeManagerInterface;
 use Tardigrades\SectionField\Service\SectionManagerInterface;
+use Tardigrades\SectionField\ValueObject\FieldConfig;
 use Tardigrades\SectionField\ValueObject\FieldTypeGeneratorConfig;
-use Tardigrades\SectionField\ValueObject\Handle;
+use Tardigrades\SectionField\ValueObject\FullyQualifiedClassName;
 use Tardigrades\SectionField\ValueObject\SectionConfig;
-use Tardigrades\SectionField\ValueObject\Version;
+use Tardigrades\SectionField\ValueObject\Type;
 
 /**
  * @coversDefaultClass Tardigrades\SectionField\Generator\DoctrineConfigGenerator
@@ -38,84 +41,232 @@ final class DoctrineConfigGeneratorTest extends TestCase
     private $sectionManager;
 
     /** @var ContainerInterface|Mockery\Mock */
-    private $containerManager;
+    private $container;
+
+    /** @var DoctrineConfigGenerator */
+    private $generator;
 
     public function setUp()
     {
         $this->fieldManager = Mockery::mock(FieldManagerInterface::class);
         $this->fieldTypeManager = Mockery::mock(FieldTypeManagerInterface::class);
         $this->sectionManager = Mockery::mock(SectionManagerInterface::class);
-        $this->containerManager = Mockery::mock(ContainerInterface::class);
+        $this->container = Mockery::mock(ContainerInterface::class);
+        $this->generator = new DoctrineConfigGenerator(
+            $this->fieldManager,
+            $this->fieldTypeManager,
+            $this->sectionManager,
+            $this->container
+        );
     }
 
     /**
      * @test
+     * @covers ::generateElements
      * @covers ::generateBySection
      */
-    public function it_should_generate_by_a_section()
+    public function it_should_generate_by_a_section_from_template()
     {
-        $sectionFields = [
-            'section' => [
-                'name' => 'I have a sexy name',
-                'handle' => 'handle',
-                'fields' => ['these', 'are', 'fields'],
-                'slug' => ['these'],
-                'default' => 'these',
-                'namespace' => 'My\Namespace'
-            ]
-        ];
+        $sectionOne = $this->givenASectionWithName('One');
 
-        $section = Mockery::mock(new Section())->makePartial();
-        $section->shouldReceive('getConfig')->andReturn(SectionConfig::fromArray($sectionFields));
-        $section->shouldReceive('getHandle')->andReturn(Handle::fromString('handle'));
-        $section->shouldReceive('getVersion')->andReturn(Version::fromInt(5));
-
-        $fieldHandle = [
-            'handle' => [
-                'name' => 'name',
-                'handle' => 'wheee',
-                'kind' => 'kid',
-                'to' => 'to'
-            ]
-        ];
-        $one = new Field();
-        $one->setFieldType(new FieldType());
-        $one->getFieldType()->setFullyQualifiedClassName('fullname');
-        $one->setName('name');
-        $one->getFieldType()->setType('atypical');
         $fieldtypeConfigDoctrine = FieldTypeGeneratorConfig::fromArray(
             [
                 'doctrine' => [
-                    'a' => new \stdClass(),
-                    'c' => new \stdClass()
+                    'oneToOne' => DoctrineOneToOneGenerator::class
                 ]
             ]
         );
 
-        $containerInter = Mockery::mock(ContainerInterface::class)->makePartial();
-        $containerInter->shouldReceive('getFieldTypeGeneratorConfig')
+        $fieldType = Mockery::mock(FieldTypeInterface::class);
+        $fieldType->shouldReceive('getFieldTypeGeneratorConfig')
+            ->once()
             ->andReturn($fieldtypeConfigDoctrine);
+        $fieldType->shouldReceive('getFullyQualifiedClassName')
+            ->andReturn(FullyQualifiedClassName::fromString('\My\Namespace\Field'));
+        $fieldType->shouldReceive('getType')->andReturn(Type::fromString('EmailType'));
+        $fieldType->shouldReceive('directory')->andReturn(__DIR__ . '/../../../../src/FieldType/Relationship');
+
+        $fieldOne = $this->givenAFieldWithNameKindAndTo('One', 'one-to-one', 'Two');
+
+        $relationships = [
+            'sectionOne' => [
+                'fieldOne' => [
+                    'kind' => 'one-to-one',
+                    'to' => 'sectionTwo',
+                    'from' => 'sectionOne',
+                    'fullyQualifiedClassName' => FullyQualifiedClassName::fromString(
+                        '\\My\\Namespace\\FieldTypeClassOne'
+                    ),
+                    'relationship-type' => 'unidirectional'
+                ]
+            ]
+        ];
 
         $this->fieldManager->shouldReceive('readByHandles')
             ->once()
-            ->andReturn([$one]);
+            ->andReturn([$fieldOne]);
 
         $this->sectionManager->shouldReceive('getRelationshipsOfAll')
             ->once()
-            ->andReturn($fieldHandle);
+            ->andReturn($relationships);
 
-        $this->containerManager->shouldReceive('get')
-            ->once()
-            ->andReturn($containerInter);
+        $this->sectionManager->shouldReceive('readByHandle')
+            ->twice()
+            ->andReturn($sectionOne);
 
-        $generator = new DoctrineConfigGenerator(
-            $this->fieldManager,
-            $this->fieldTypeManager,
-            $this->sectionManager,
-            $this->containerManager
+        $this->container->shouldReceive('get')
+            ->twice()
+            ->andReturn($fieldType);
+
+        $writable = $this->generator->generateBySection($sectionOne);
+        $this->assertInstanceOf(Writable::class, $writable);
+        $this->assertSame("My\\Namespace\\Resources\\config\\doctrine\\", $writable->getNamespace());
+        $this->assertSame("SectionOne.orm.xml", $writable->getFilename());
+        $this->assertSame($this->givenXmlResult(), $writable->getTemplate());
+        $this->assertCount(0, $this->generator->getBuildMessages());
+    }
+
+    /**
+     * @test
+     * @covers ::generateElements
+     * @covers ::generateBySection
+     */
+    public function it_should_generate_section_and_skip_to_catch_block()
+    {
+        $sectionOne = $this->givenASectionWithName('One');
+
+        $fieldtypeConfigDoctrine = FieldTypeGeneratorConfig::fromArray(
+            [
+                'doctrine' => [
+                    'oneToOne' => DoctrineOneToOneGenerator::class
+                ]
+            ]
         );
 
-        $writable = $generator->generateBySection($section);
+        $fieldType = Mockery::mock(FieldTypeInterface::class);
+        $fieldType->shouldReceive('getFieldTypeGeneratorConfig')
+            ->once()
+            ->andReturn($fieldtypeConfigDoctrine);
+        $fieldType->shouldReceive('getFullyQualifiedClassName')
+            ->andReturn(FullyQualifiedClassName::fromString('\My\Namespace\Field'));
+        $fieldType->shouldReceive('getType')->andReturn(Type::fromString('Email'));
+
+        $fieldOne = $this->givenAFieldWithNameKindAndTo('One', 'one-to-one', 'Two');
+
+        $relationships = [
+            'sectionOne' => [
+                'fieldOne' => [
+                    'kind' => 'one-to-one',
+                    'to' => 'sectionTwo',
+                    'from' => 'sectionOne',
+                    'fullyQualifiedClassName' => FullyQualifiedClassName::fromString(
+                        '\\My\\Namespace\\FieldTypeClassOne'
+                    ),
+                    'relationship-type' => 'unidirectional'
+                ]
+            ]
+        ];
+
+        $this->fieldManager->shouldReceive('readByHandles')
+            ->once()
+            ->andReturn([$fieldOne]);
+
+        $this->sectionManager->shouldReceive('getRelationshipsOfAll')
+            ->once()
+            ->andReturn($relationships);
+
+        $this->container->shouldReceive('get')
+            ->twice()
+            ->andReturn($fieldType);
+
+        $writable = $this->generator->generateBySection($sectionOne);
         $this->assertInstanceOf(Writable::class, $writable);
+        $this->assertSame("My\\Namespace\\Resources\\config\\doctrine\\", $writable->getNamespace());
+        $this->assertSame("SectionOne.orm.xml", $writable->getFilename());
+        $this->assertSame($this->givenXmlResult(), $writable->getTemplate());
+        $this->assertCount(1, $this->generator->getBuildMessages());
+    }
+
+    private function givenASectionWithName($name)
+    {
+        $sectionName = 'Section ' . $name;
+        $sectionHandle = 'section' . $name;
+
+        $sectionConfig = SectionConfig::fromArray([
+            'section' => [
+                'name' => $sectionName,
+                'handle' => $sectionHandle,
+                'fields' => [
+                    'title',
+                    'body',
+                    'created'
+                ],
+                'slug' => ['title'],
+                'default' => 'title',
+                'namespace' => 'My\\Namespace'
+            ]
+        ]);
+
+        $section = new Section();
+
+        $section->setName($sectionName);
+        $section->setHandle($sectionHandle);
+        $section->setConfig($sectionConfig->toArray());
+        $section->setVersion(1);
+        $section->setCreated(new \DateTime());
+        $section->setUpdated(new \DateTime());
+
+        return $section;
+    }
+
+    private function givenAFieldWithNameKindAndTo($name, $kind, $to)
+    {
+        $fieldName = 'Field ' . $name;
+        $fieldHandle = 'field' . $name;
+        $field = new Field();
+        $field->setName($fieldName);
+        $field->setHandle($fieldHandle);
+
+        $fieldConfig = FieldConfig::fromArray([
+            'field' => [
+                'name' => $fieldName,
+                'handle' => $fieldHandle,
+                'kind' => $kind,
+                'to' => 'section' . $to,
+                'relationship-type' => 'unidirectional'
+            ]
+        ]);
+
+        $field->setConfig($fieldConfig->toArray());
+
+        $fieldType = new FieldType();
+        $fieldType->setFullyQualifiedClassName('\\My\\Namespace\\FieldTypeClass' . $name);
+        $fieldType->setType('Email');
+
+        $field->setFieldType($fieldType);
+
+        return $field;
+    }
+
+    private function givenXmlResult()
+    {
+        //@codingStandardsIgnoreStart
+        $expected = <<<TXT
+<?xml version="1.0"?>
+<doctrine-mapping xmlns="http://doctrine-project.org/schemas/orm/doctrine-mapping" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://doctrine-project.org/schemas/orm/doctrine-mapping http://raw.github.com/doctrine/doctrine2/master/doctrine-mapping.xsd">
+  <entity name="My\Namespace\Entity\SectionOne" table="sectionOne">
+    <lifecycle-callbacks>
+      <lifecycle-callback type="prePersist" method="onPrePersist"/>
+      <lifecycle-callback type="preUpdate" method="onPreUpdate"/>
+    </lifecycle-callbacks>
+    <id name="id" type="integer">
+      <generator strategy="AUTO"/>
+    </id>
+  </entity>
+</doctrine-mapping>
+
+TXT;
+        //@codingStandardsIgnoreEnd
+        return $expected;
     }
 }
